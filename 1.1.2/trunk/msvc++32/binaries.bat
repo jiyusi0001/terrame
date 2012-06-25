53618ac5a0097c956bd1bf04659b7f9f07b7a666
reisrodrigo <reisrodrigo@199acd58-1fde-4085-87da-56931eaf1e6f>
2012-06-25 17:59:52 +0000

    Código estável 1.1.2
    git-svn-id: http://192.168.0.234/svn/terrame/tags@6 199acd58-1fde-4085-87da-56931eaf1e6f

---
+++ 1.1.2/trunk/msvc++32/binaries.bat
@@ -0,0 +1,35 @@
+REM rmdir /S /Q ..\..\bin\
+REM mkdir ..\..\bin\
+
+copy /Y release\TerraME.exe ..\..\bin\
+
+REM copy /Y ..\..\dependencies\qt\lib\QtCore4.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\qt\lib\QtGui4.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\qt\lib\QtNetwork4.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\qwt\lib\qwt5.dll ..\..\bin\
+
+REM REM copy /Y ..\..\dependencies\terralib\Release\shapelib.dll ..\..\bin\
+REM REM copy /Y ..\..\dependencies\terralib\Release\te_ado.dll ..\..\bin\
+REM REM copy /Y ..\..\dependencies\terralib\Release\te_mysql.dll ..\..\bin\
+REM REM copy /Y ..\..\dependencies\terralib\Release\te_utils.dll ..\..\bin\
+REM REM copy /Y ..\..\dependencies\terralib\Release\te_functions.dll ..\..\bin\
+REM REM copy /Y ..\..\dependencies\terralib\Release\tiff.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\terralib\Release\terralib.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\terralib\Release\terralib_ado.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\terralib\Release\terralib_shp.dll ..\..\bin\
+
+REM copy /Y ..\..\dependencies\mysql\libmysql.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\lua\lib\lua5.1.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\msvc2010\msvcp100.dll ..\..\bin\
+REM copy /Y ..\..\dependencies\msvc2010\msvcr100.dll ..\..\bin\
+
+REM REM copy /Y ..\..\dependencies\luagd\*.* ..\..\bin\
+REM REM copy /Y ..\..\dependencies\zlib\zlib1.dll ..\..\bin\
+REM mkdir ..\..\bin\Lua
+REM copy /Y ..\..\src\lua\*.lua ..\..\bin\Lua
+
+REM mkdir ..\..\bin\database
+REM copy /Y ..\..\database ..\..\bin\database\
+
+REM mkdir ..\..\bin\demos
+REM copy /Y ..\..\demos ..\..\bin\demos\
